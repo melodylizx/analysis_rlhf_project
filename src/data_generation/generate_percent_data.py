11c2170cd579449824419a31247c4e6c8eda604e
InesArous <ines@exascale.info>
2024-01-08 14:45:53 -0500

    rectifying code for percent split

--- src/data_generation/generate_percent_data.py
+++ src/data_generation/generate_percent_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from utils import create_directory
 # Load the dataset
 train_comparisons = pd.read_parquet('../../data/reliability/perfect/train_perfect.parquet')
 valid_comparisons = pd.read_parquet('../../data/reliability/perfect/validation_perfect.parquet')
@@ -7,21 +7,18 @@ valid_comparisons = pd.read_parquet('../../data/reliability/perfect/validation_p
 # Define the percentages to be used for training and validation
 percentages = [0.01, 0.05, 0.10, 0.20, 0.50, 0.80]
 directory_path = '../../data/percent'
-
+create_directory(directory_path)
 # Generate datasets for each percentage case
 def get_percent_dataset(pct):
-    # Calculate the number of samples for training and validation
-    num_train = int(pct * len(train_comparisons))
-    num_valid = int(pct * len(valid_comparisons))
-
     # Sample the dataset
-    return_train = train_comparisons.sample(n=num_train)
-    return_valid = valid_comparisons.sample(n=num_train)
+    return_train = train_comparisons.sample(frac=pct,random_state=42)
+    return_valid = valid_comparisons.sample(frac=pct,random_state=42)
     
     return return_train,return_valid
 
 for pct in percentages:
     return_train,return_valid = get_percent_dataset(pct)
+    create_directory(directory_path+"/" + str(pct))
     return_train.to_parquet(directory_path +"/" + str(pct) +'/train_'+ str(pct) +'.parquet')
     return_valid.to_parquet(directory_path + "/" + str(pct) +'/validation_'+str(pct) +'.parquet')
 

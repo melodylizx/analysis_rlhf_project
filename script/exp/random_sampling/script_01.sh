9dd4926a0f7c58538a2661452595c097a3bab3ae
InesArous <ines@exascale.info>
2024-11-13 09:42:39 -0500

    remove extra files

--- script/exp/random_sampling/script_01.sh
+++ script/exp/random_sampling/script_01.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 #SBATCH --partition=long                     # Ask for unkillable job
-#SBATCH --gres=gpu:a100l:2 --constraint="dgx&ampere"                                     
-#SBATCH --mem=96G                                        # Ask for 10 GB of RAM
+#SBATCH --gres=gpu:1 --constraint="dgx&ampere"
+#SBATCH --mem=48G                                        # Ask for 10 GB of RAM
 #SBATCH --time=48:00:00                                   # The job will run for 3 hours
-#SBATCH --output=./logs/01_out.txt
-#SBATCH --error=./logs/01_error.txt
-#SBATCH --constraint=80gb
+#SBATCH --output=./logs/random/01_out.txt
+#SBATCH --error=./logs/random/01_error.txt
 #SBATCH -c 2
 
 
 # 1. Load the required modules
 module --quiet load anaconda/3
-conda activate "rlhf"
+conda init
+conda activate "vrlhf"
 
 cd ./src/training/
 foldername=$(date +%Y_%m_%d_%H_%M)
-CHPTPATH=/network/scratch/i/ines.arous/experiment_reward_model/01/"$foldername"
+CHPTPATH=/network/scratch/i/ines.arous/experiment_reward_model/random_sampling/01/"$foldername"
+SAVEPATH=/network/scratch/i/ines.arous/ppo/random_sampling/01/"$foldername"
 mkdir -p ${CHPTPATH}
+mkdir -p ${SAVEPATH}
 
 
 deepspeed ./reward_model/train_reward_model_gptj.py --local_rank=0 --seed=3  --data_path="/network/scratch/i/ines.arous/data_rlhf/percent/0.01/"  --chpt_path="${CHPTPATH}"
 python ./reward_model/gptj_reward_test.py --ckpt_path="${CHPTPATH}"
+accelerate launch --config_file configs/default_accelerate_config.yaml new_ppo.py --ckpt_path="${CHPTPATH}" --save_path="${SAVEPATH}"
